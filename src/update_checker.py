0a346b2b6dff86eb9c5d97025c80321cc3f65314
NizamLZ <NizamLZ>
2019-08-02 23:26:06 +0530

    Try catch to supress unexpected error incase of network failure etc.

--- src/update_checker.py
+++ src/update_checker.py
@@ -37,27 +37,30 @@ class UpdateChecker(threading.Thread):
         self._current_version = current_version
 
     def run(self):
-        response = requests.get(GITHUB_RELEASES_API, timeout=30)
-        if response.status_code == 200:
-            json_response = response.json()
-            latest_version = json_response["tag_name"]
-            print(latest_version)
-            if version.parse(latest_version[1:]) > version.parse(self._current_version[1:]):
-                download_url = ''
-                download_name = ''
-                for asset in json_response['assets']:
-                    if OS_MAP[sys.platform] in asset['name'].lower():
-                        download_url = asset['browser_download_url']
-                        download_name = asset['name']
-                        break
+        try:
+            response = requests.get(GITHUB_RELEASES_API, timeout=30)
+            if response.status_code == 200:
+                json_response = response.json()
+                latest_version = json_response["tag_name"]
+                print(latest_version)
+                if version.parse(latest_version[1:]) > version.parse(self._current_version[1:]):
+                    download_url = ''
+                    download_name = ''
+                    for asset in json_response['assets']:
+                        if OS_MAP[sys.platform] in asset['name'].lower():
+                            download_url = asset['browser_download_url']
+                            download_name = asset['name']
+                            break
 
-                wx.PostEvent(self._parent, UpdateCheckDoneEvent({
-                    'update_available': True,
-                    'latest_version': latest_version,
-                    'download_url': download_url,
-                    'download_name': download_name
-                }))
-            else:
-                wx.PostEvent(self._parent, UpdateCheckDoneEvent({
-                    'update_available': False
-                }))
+                    wx.PostEvent(self._parent, UpdateCheckDoneEvent({
+                        'update_available': True,
+                        'latest_version': latest_version,
+                        'download_url': download_url,
+                        'download_name': download_name
+                    }))
+                else:
+                    wx.PostEvent(self._parent, UpdateCheckDoneEvent({
+                        'update_available': False
+                    }))
+        except Exception as ex:
+            pass
